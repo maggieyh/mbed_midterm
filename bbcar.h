46ba6cc88b1a699cb9bf5cd24c86743158381cd0
yaohsiao <yaohsiaopid@gapp.nthu.edu.tw>
2018-04-27 14:59:12 +0800

    init methods

--- bbcar.h
+++ bbcar.h
@@ -1,9 +1,11 @@
 #include "parallax.h"
+#include "mbed.h"
 extern parallax_servo *servo0_ptr, *servo1_ptr;
 extern Ticker servo_ticker;
 extern Ticker encoder_ticker;
 extern parallax_encoder *encoder3_ptr;
-
+extern Serial xbee;
+extern Serial pc;
 class MyCar 
 {
     public:
@@ -24,7 +26,105 @@ class MyCar
             servo1_ptr->set_ramp(1500);
             
             encoder_ticker.attach(&encoder_control, .01);
-    
+            
+            
         }
-        
+        float readEncoder() {
+            return (float)(*encoder3_ptr);
+        }
+
+        void ServoStop(){
+            servo0_ptr->set_speed(0);
+            servo1_ptr->set_speed(0);
+            servo0_ptr->set_factor(1);
+            servo1_ptr->set_factor(1);
+            return;
+        }
+
+        void ServoCtrl( int speed ){
+            servo0_ptr->set_speed(speed*0.3);
+            servo1_ptr->set_speed(-speed*0.7);
+            return;
+        }
+
+        void ServoTurn( int speed ){
+            servo0_ptr->set_speed(speed*0.3);
+            servo1_ptr->set_speed(speed*0.7);
+        }
+
+        void PointToPoint(float x, float y) {
+
+        }
+
+        void default_sketch1() {
+
+        }
 };
+
+
+
+
+
+void reply_messange(char *xbee_reply, char *messange){
+   xbee_reply[0] = xbee.getc();
+   xbee_reply[1] = xbee.getc();
+   xbee_reply[2] = xbee.getc();
+   if(xbee_reply[1] == 'O' && xbee_reply[2] == 'K'){
+    pc.printf("%s\r\n", messange);
+    xbee_reply[0] = '\0';
+    xbee_reply[1] = '\0';
+    xbee_reply[2] = '\0';
+   }
+ }
+ 
+ void check_addr(char *xbee_reply, char *messenger){
+   xbee_reply[0] = xbee.getc();
+   xbee_reply[1] = xbee.getc();
+   xbee_reply[2] = xbee.getc();
+   pc.printf("%s = %c%c\r\n", messenger, xbee_reply[1], xbee_reply[2]);
+   xbee_reply[0] = '\0';
+   xbee_reply[1] = '\0';
+   xbee_reply[2] = '\0';
+ }
+
+ void xbeeConnect() 
+ {
+   
+    pc.baud(9600);
+
+    char xbee_reply[3];
+
+    // XBee setting
+    xbee.baud(9600);
+    xbee.printf("+++");
+    xbee_reply[0] = xbee.getc();
+    xbee_reply[1] = xbee.getc();
+    if(xbee_reply[0] == 'O' && xbee_reply[1] == 'K'){
+        pc.printf("enter AT mode.\r\n");
+        xbee_reply[0] = '\0';
+        xbee_reply[1] = '\0';
+    }
+    xbee.printf("ATMY 0x60\r\n");
+    reply_messange(xbee_reply, "setting MY : 0x60");
+
+    xbee.printf("ATDL 0x61\r\n");
+    reply_messange(xbee_reply, "setting DL : 0x62");
+
+    xbee.printf("ATWR\r\n");
+    reply_messange(xbee_reply, "write config");
+
+    xbee.printf("ATMY\r\n");
+    check_addr(xbee_reply, "MY");
+
+    xbee.printf("ATDL\r\n");
+    check_addr(xbee_reply, "DL");
+
+    xbee.printf("ATCN\r\n");
+    reply_messange(xbee_reply, "exit AT mode");
+    xbee.getc();
+
+    // start
+    pc.printf("start\r\n");
+    char buf[100] = {0};
+
+}
--- main.cpp
+++ main.cpp
@@ -1,41 +1,84 @@
 #include "mbed.h"
 #include "glibr.h"
-// #include "parallax.h"
+#include "mbed_rpc.h"
 #include "bbcar.h"
+#include "uLCD_4DGL.h"
+
+uLCD_4DGL uLCD(D1, D0, D2);
 glibr GSensor(D14,D15);
 Serial pc(USBTX, USBRX);
+Serial xbee(D9, D7);
 PwmOut pin11(D11), pin12(D12);
 DigitalIn pin3(D3);
 DigitalOut p(LED1);
+
+
 
 parallax_encoder *encoder3_ptr;
 parallax_servo *servo0_ptr, *servo1_ptr;
 int isr_flag = 0;
 
+MyCar BBCar;
+void sys_init();
+void gesture_handler();
+
+Ticker gesture_ticker;
 
 int main(void)
 {
-    p = 0;
+    sys_init();
+
     
+    /*
+    xbeeConnect();
+    int i;
+    char buf[100] = {0};
+
+    while(1) 
+    {
+        i = 0;
+        xbee.getc();
+        while(i<4) {
+            buf[i] = xbee.getc();
+            i++;
+            buf[i]='\0';
+        }
+        xbee.printf("%s", buf);
+        wait(0.1);
+        i = 0; 
+        while(buf[i] != '\0'){
+           buf[i] = 0;
+           i++;
+         }
+    }*/
+}
+
+
+void sys_init() {
+    BBCar.init(pin11, pin12, pin3);
+
     // Initialize Sensor with I2C
     if ( GSensor.ginit() ) {
+        p = 1;
         pc.printf("APDS-9960 initialization complete\r\n");
     } else {
+        p = 0;
         pc.printf("Something went wrong during APDS-9960 init\r\n");
-        return -1;
     }
- 
+    
     // Start running the APDS-9960 gesture sensor engine
     if ( GSensor.enableGestureSensor(true) ) {
         pc.printf("Gesture sensor is now running\r\n");
     } else {
         pc.printf("Something went wrong during gesture sensor init!\r\n");
-        return -1;
     }
+
+    gesture_ticker.attach(&gesture_handler, .01);
     
-    while(1) {
-        //pc.printf(".....\r\n");
-        if ( GSensor.isGestureAvailable() ) {         // gesture detect
+}
+void gesture_handler()
+{
+    if ( GSensor.isGestureAvailable() ) {         // gesture detect
             switch ( GSensor.readGesture() ) {        // gesture differentiate
                 case DIR_UP:
                     pc.printf("UP\r\n");
@@ -60,6 +103,4 @@ int main(void)
                     break;
             }
         }
-    }    
 }
-
