cb131756fd060d654c336a33901d0e481cbe3257
yaohsiao <yaohsiaopid@gapp.nthu.edu.tw>
2018-04-28 16:32:55 +0800

    ir gesture,

--- main.cpp
+++ main.cpp
@@ -1,66 +1,42 @@
 #include "mbed.h"
-// #include "glibr.h"
-// #include "mbed_rpc.h"
-// #include "bbcar.h"
+#include "glibr.h"
 #include "parallax.h"
-// #include "uLCD_4DGL.h"
+#include "uLCD_4DGL.h"
 #include <cmath>      
 #define PI 3.14159265
 
-// uLCD_4DGL uLCD(D1, D0, D2);
-// glibr GSensor(D14,D15);
+uLCD_4DGL uLCD(D1, D0, D2);
+glibr GSensor(D14,D15);
 Serial pc(USBTX, USBRX);
 Serial xbee(D9, D7);
 PwmOut pin11(D11), pin12(D12);
 DigitalIn pin3(D3), pin2(D2);
 DigitalOut p(LED1);
-
+InterruptIn sw2btn(SW2);
 int isr_flag = 0;
+int mode = 0; // remote control or local demo
+
 void sys_init();
 void gesture_handler();
 void xbeeConnect();
 void reply_messange(char *xbee_reply, char *messange);
 void check_addr(char *xbee_reply, char *messenger);
+void mode_switch();
 Ticker gesture_ticker;
 
 float car_x, car_y, car_theta;
 parallax_servo *servo0_ptr, *servo1_ptr;
 parallax_encoder *encoder3_ptr, *encoder2_ptr;
 
-void car_init(PwmOut &pin_servo0, PwmOut &pin_servo1,  DigitalIn &d3, DigitalIn &d2) {
-    static parallax_servo servo0(pin_servo0);
-    static parallax_servo servo1(pin_servo1);
-    static parallax_encoder encoder3(d3);
-    static parallax_encoder encoder2(d2);
-    servo0_ptr = &servo0;
-    servo1_ptr = &servo1;
-    encoder3_ptr = &encoder3;
-    encoder2_ptr = &encoder2;
-
-    servo_ticker.attach(&servo_control, .5);
-    servo0 = 0; servo1 = 0;
-    servo0_ptr->set_ramp(100);
-    servo1_ptr->set_ramp(1500);
-    
-    encoder_ticker.attach(&encoder_control, .01);
-    
-    car_x = 0;
-    car_y = 0;
-    car_theta = 0;
-}
+void car_init(PwmOut &pin_servo0, PwmOut &pin_servo1,  DigitalIn &d3, DigitalIn &d2);
 void ServoCtrl( int speed ){
     servo0_ptr->set_speed(speed*0.3);
     servo1_ptr->set_speed(-speed*0.7);
     return;
 }
 
-void ServoStop(){
-    servo0_ptr->set_speed(0);
-    servo1_ptr->set_speed(0);
-    servo0_ptr->set_factor(1);
-    servo1_ptr->set_factor(1);
-    return;
-}
+void ServoStop();
+
 void ServoDistaqnce(float distance) 
 {
     encoder3_ptr->reset();
@@ -69,49 +45,9 @@ void ServoDistaqnce(float distance)
     ServoStop();
 }
 
-void ServoTurn(float deg){
-    int speed = -60;
-    encoder3_ptr->reset();
-    if (deg < 0) { speed = -speed; deg = -deg;}
-    //25 encoder 3
-    servo0_ptr->set_speed(speed*0.5);
-    servo1_ptr->set_speed(speed*0.7);
-    float del = abs(encoder3_ptr->get_cm() - deg * 31.5 / 360.0);
-    while(encoder3_ptr->get_cm() < deg * 31.5 / 360.0) {
-        // if(abs(encoder3_ptr->get_cm() - deg * 31 / 360.0) < 0.1 * del) {
-        //     speed = speed / 3;
-        //     servo0_ptr->set_speed(speed*0.4);
-        //     servo1_ptr->set_speed(speed*0.7);
-        // }
-        wait_ms(3);
-    } 
-    ServoStop();
-}
+void ServoTurn(float deg);
 
-void PointToPoint(float x, float y) {
-    float deltax = x - car_x, deltay = y - car_y;
-    
-    float target = 0;
-    if (deltax >= 0 && deltay >= 0) {
-        target = atan (deltay/deltax) * 180.0 / PI;
-    } else if (deltax >= 0 && deltay <= 0) {
-        target = 360+atan (deltay/deltax) * 180.0 / PI;
-    } else {
-        target = 180+atan (deltay/deltax) * 180.0 / PI;
-    }
-     
-    float turnDeg = target - car_theta ;
-    float distance = sqrt(pow(deltax, 2.0)+pow(deltay,2.0)); 
-    
-    ServoTurn(turnDeg);
-    wait(0.5);
-    ServoDistaqnce(distance);
-    car_x = x;
-    car_y = y;
-    car_theta = target;
-    pc.printf("%f | %f  | car_theta : %f \r\n", turnDeg, distance, car_theta);
-}
-
+void PointToPoint(float x, float y);
 
 int main(void)
 {
@@ -157,27 +93,156 @@ int main(void)
     }*/
 }
 
+void ServoTurn(float deg){
+    int speed = -60;
+    encoder3_ptr->reset();
+    if (deg < 0) { speed = -speed; deg = -deg;}
+    //25 encoder 3
+    servo0_ptr->set_speed(speed*0.5);
+    servo1_ptr->set_speed(speed*0.7);
+    float del = abs(encoder3_ptr->get_cm() - deg * 31.5 / 360.0);
+    while(encoder3_ptr->get_cm() < deg * 31.5 / 360.0) {
+        // if(abs(encoder3_ptr->get_cm() - deg * 31 / 360.0) < 0.1 * del) {
+        //     speed = speed / 3;
+        //     servo0_ptr->set_speed(speed*0.4);
+        //     servo1_ptr->set_speed(speed*0.7);
+        // }
+        wait_ms(3);
+    } 
+    ServoStop();
+}
 
+void car_init(PwmOut &pin_servo0, PwmOut &pin_servo1,  DigitalIn &d3, DigitalIn &d2) {
+    static parallax_servo servo0(pin_servo0);
+    static parallax_servo servo1(pin_servo1);
+    static parallax_encoder encoder3(d3);
+    static parallax_encoder encoder2(d2);
+    servo0_ptr = &servo0;
+    servo1_ptr = &servo1;
+    encoder3_ptr = &encoder3;
+    encoder2_ptr = &encoder2;
+
+    servo_ticker.attach(&servo_control, .5);
+    servo0 = 0; servo1 = 0;
+    servo0_ptr->set_ramp(100);
+    servo1_ptr->set_ramp(1500);
+    
+    encoder_ticker.attach(&encoder_control, .01);
+    
+    car_x = 0;
+    car_y = 0;
+    car_theta = 0;
+}
+
+void ServoStop(){
+    servo0_ptr->set_speed(0);
+    servo1_ptr->set_speed(0);
+    servo0_ptr->set_factor(1);
+    servo1_ptr->set_factor(1);
+    return;
+}
+
+void displayMenu() 
+{
+
+}
+
+
+void PointToPoint(float x, float y) {
+    float deltax = x - car_x, deltay = y - car_y;
+    
+    float target = 0;
+    if (deltax >= 0 && deltay >= 0) {
+        target = atan (deltay/deltax) * 180.0 / PI;
+    } else if (deltax >= 0 && deltay <= 0) {
+        target = 360+atan (deltay/deltax) * 180.0 / PI;
+    } else {
+        target = 180+atan (deltay/deltax) * 180.0 / PI;
+    }
+     
+    float turnDeg = target - car_theta ;
+    float distance = sqrt(pow(deltax, 2.0)+pow(deltay,2.0)); 
+    
+    ServoTurn(turnDeg);
+    wait(0.5);
+    ServoDistaqnce(distance);
+    car_x = x;
+    car_y = y;
+    car_theta = target;
+    pc.printf("%f | %f  | car_theta : %f \r\n", turnDeg, distance, car_theta);
+}
+void mode_switch() 
+{
+    pc.printf("jjj");
+    ServoStop();
+    mode = mode? 0:1;
+    if (mode) {
+        // in local control
+        uLCD.cls();
+        uLCD.printf("Menu^\n1.Happy face\n2.Sad face\n3.Square");
+    }  else {
+        // remote control
+        xbeeConnect();
+    }
+}
 void sys_init() {
     car_init(pin11, pin12, pin3, pin2);
-    xbeeConnect();
     // Initialize Sensor with I2C
-    // if ( GSensor.ginit() ) {
-    //     p = 1;
-    //     pc.printf("APDS-9960 initialization complete\r\n");
-    // } else {
-    //     p = 0;
-    //     pc.printf("Something went wrong during APDS-9960 init\r\n");
-    // }
-    
-    // // Start running the APDS-9960 gesture sensor engine
-    // if ( GSensor.enableGestureSensor(true) ) {
-    //     pc.printf("Gesture sensor is now running\r\n");
-    // } else {
-    //     pc.printf("Something went wrong during gesture sensor init!\r\n");
-    // }
-
+    int succ = 0;
+    if ( GSensor.ginit() ) {
+        uLCD.printf("APDS-9960 init\n");
+        succ = 1;
+    } else {
+        uLCD.printf("APDS-9960 fail\n");
+    }
+    // Start running the APDS-9960 gesture sensor engine
+    if ( GSensor.enableGestureSensor(true) ) {
+        succ = 1;
+        uLCD.printf("Gesture enable\r\n");
+    } else {
+        uLCD.printf("Gesture fail?!\r\n");
+    }
     // gesture_ticker.attach(&gesture_handler, .01);
+    if (succ) {
+        uLCD.cls();
+        uLCD.printf("Welcome\nChoose Mode(Up/Down to move cursor, Right to confirm)\n\n0. Remote\n1. Local\n");
+        uLCD.locate(0,9);
+        uLCD.printf("%d", mode);
+        int r = 0;
+        while(1) {
+            if ( GSensor.isGestureAvailable() ) {    
+                
+                switch ( GSensor.readGesture() ) {        // gesture differentiate
+                    case DIR_UP:
+                        // uLCD.printf("UP\n");
+                        pc.printf("UP\r\n");
+                        mode = (mode + 1) % 2;
+                        break;
+                    case DIR_DOWN:
+                        // uLCD.printf("DOWN\n");
+                        pc.printf("DOWN\r\n");
+                        mode = abs((mode - 1) % 2);
+                        break;
+                    case DIR_RIGHT:
+                        uLCD.printf("RIGHT\n");
+                        pc.printf("RIGHT\r\n");
+                        // r=1;
+                        break;
+                    default:
+                        pc.printf("NONE\r\n");
+                        break;
+                }
+                uLCD.locate(0,9);
+                uLCD.printf("%d", mode);
+             }
+        }
+        uLCD.printf("jjjjjjj");
+        
+
+        
+    }
+    
+    
     
 }
 
@@ -250,23 +315,24 @@ void xbeeConnect() {
     char buf[100] = {0};
     int i = 0;
 
-        xbee.getc();
-        while (i < 4){
-          buf[i] = xbee.getc();
-          i++;
-          buf[i] = '\0';
-        }
-        xbee.printf("%s", buf);
-        wait(0.1);
+    xbee.getc();
+    while (i < 4 || sw2btn == 1){
+        buf[i] = xbee.getc();
+        i++;
+        buf[i] = '\0';
+    }
+    xbee.printf("%s", buf);
+    wait(0.1);
 
-        i = 0;
-        while(buf[i] != '\0'){
-          buf[i] = 0;
-          i++;
-        }
+    i = 0;
+    while(buf[i] != '\0'){
+        buf[i] = 0;
+        i++;
+    }
+    uLCD.printf("\nConnected!\n");
 }
 
-/*
+
 void gesture_handler()
 {
     if ( GSensor.isGestureAvailable() ) {         // gesture detect
@@ -295,4 +361,3 @@ void gesture_handler()
             }
         }
 }
-*/
